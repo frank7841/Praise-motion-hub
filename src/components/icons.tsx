14fed07be051e410c4f8a3cee4aaea398ce8a89a
Shanice Singira <shanicesingira01@gmail.com>
2026-01-05 17:36:25 +0000

    tiktok icon (_for element svg_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -51,7 +51,7 @@ export const SpotifyIcon = (props: SVGProps<SVGSVGElement>) => (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="none"
+    fill="currentColor"
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
